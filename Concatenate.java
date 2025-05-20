52ff9162a52a802e8bcdd3f30640cfd41dd4c054
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-05-20 16:38:58 +0600

    Create Concatenate.java

---
+++ Concatenate.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        String name0 = "Md.Ferdous";
+        String name1 = " Foysal ";
+        String name2 = "Hridoy";
+        String name4 = name0 + name1 + name2;
+        System.out.println(name4);
+    }
+}
