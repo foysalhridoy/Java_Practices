853873d779810e6a465a09c86df159a3b4bb4f7d
Md. Ferdous Foysal Hridoy <ffoysalhridoy@gmail.com>
2025-05-20 16:45:51 +0600

    CharAt.java

---
+++ CharAt.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        String name = "Hridoy";
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        System.out.println(name.charAt(2));
+        System.out.println(name.charAt(3));
+        System.out.println(name.charAt(4));
+        System.out.println(name.charAt(5));
+    }
+}
